229c70ff5d669b8784475ad523d079e81bc6545a
Boris Ivanov <bors2908@gmail.com>
2026-04-10 18:03:15 +0300

    Add default mining values to example

--- examples/randomx_miner.js
+++ examples/randomx_miner.js
@@ -1,3 +1,11 @@
-const { mine } = require('../pkg-randomx.js-shared/dist/cjs/index')
+const {mine} = require('../pkg-randomx.js-shared/dist/cjs/index')
 
-mine()
+const job = {
+    "blob": "1010f7f2e4b70618f1fe647153b5a337098080ed8f90eee987ad4dd78bc0f3aa59bf3a53c991660000000081b6ec64f730565a0ac1dd619427aa884bf4cf8aeef7f901b00074d8390c075847",
+    "job_id": "785297",
+    "target": "ffff0000",
+    "height": 3248070,
+    "seed_hash": "3b0d5af1cdc3827e2f42f03e93661a252086f8d4e35dc65a9d3ea48e240cc795"
+}
+
+mine(job)
